a8ef27866c697b7278038a112502a263c37fb96b
SanglePankajB <pankajsangle15@gmail.com>
2025-06-19 19:27:55 +0530

    some changes

--- Questions on array/Vectors/VOfVDeclaration.cpp
+++ Questions on array/Vectors/VOfVDeclaration.cpp
@@ -15,7 +15,7 @@ int main()
     // vector of vector auto declaration 
 
     vector<vector<bool>> v(4, vector<bool>(4,0));    // IMP Line
-    // printing every element of contained vector v[i] is 1 vector insise vec V & v[i][j] is element of that Vec.
+    // printing every element of contained vector v[i] is 1 vector inside vec V & v[i][j] is element of that Vec.
     for (int i = 0; i < 4; i++)
     {
         for(int j=0;j<4;j++){
